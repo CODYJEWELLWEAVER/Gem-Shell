772f85a6375da8889bf8da4650e0714f6cc22b69
Cody Weaver <cody.weaver@colorado.edu>
2025-07-21 11:33:33 -0600

    Update secret storage method.

--- shell/config/weather.py
+++ shell/config/weather.py
@@ -1,3 +1,4 @@
-from API_KEYS import OPEN_WEATHER_MAPS
+from secrets import WEATHER_API_KEY
 
-WEATHER_API_URL = f"https://api.openweathermap.org/data/2.5/weather?zip=81647,us&units=imperial&appid={OPEN_WEATHER_MAPS}"
+WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather?zip=81647," \
+                        + f"us&units=imperial&appid={WEATHER_API_KEY}"
---
+++ shell/secrets.py
@@ -0,0 +1,10 @@
+import os
+from loguru import logger
+
+# Open Weather Maps
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY", default="")
+if WEATHER_API_KEY == "":
+    logger.warning(
+        "API key needed for weather module not found. Export an environment variable" \
+        + " named WEATHER_API_KEY with your key for Open Weather Maps."
+    )
