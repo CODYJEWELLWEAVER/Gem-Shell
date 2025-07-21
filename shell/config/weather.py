06b2dbdcb1e116647de47e14ee469785f012b013
Cody Weaver <cody.weaver@colorado.edu>
2025-07-21 11:40:04 -0600

    Move secrets.py to util.

--- shell/config/weather.py
+++ shell/config/weather.py
@@ -1,4 +1,4 @@
-from secrets import WEATHER_API_KEY
+from util.secrets import WEATHER_API_KEY
 
 WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/weather?zip=81647," \
                         + f"us&units=imperial&appid={WEATHER_API_KEY}"
